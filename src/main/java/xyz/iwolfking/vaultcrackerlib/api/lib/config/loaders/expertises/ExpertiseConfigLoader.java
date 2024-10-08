0961513956bd8b7c656dd7bb61ccba15ce1415b3
Kyle Robertson <me@iwolfking.xyz>
2024-10-08 09:34:26 -0500

    Expertise and Expertise GUI

---
+++ src/main/java/xyz/iwolfking/vaultcrackerlib/api/lib/config/loaders/expertises/ExpertiseConfigLoader.java
@@ -0,0 +1,24 @@
+package xyz.iwolfking.vaultcrackerlib.api.lib.config.loaders.expertises;
+
+import iskallia.vault.config.ExpertisesConfig;
+import iskallia.vault.config.TrinketConfig;
+import iskallia.vault.init.ModConfigs;
+import net.minecraft.resources.ResourceLocation;
+import xyz.iwolfking.vaultcrackerlib.api.events.VaultConfigEvent;
+import xyz.iwolfking.vaultcrackerlib.api.lib.loaders.VaultConfigDataLoader;
+
+import java.util.HashMap;
+
+public class ExpertiseConfigLoader extends VaultConfigDataLoader<ExpertisesConfig> {
+
+    public ExpertiseConfigLoader(String namespace) {
+        super(new ExpertisesConfig(), "expertise/expertises", new HashMap<>(), namespace);
+    }
+
+    @Override
+    public void afterConfigsLoad(VaultConfigEvent.End event) {
+        for(ExpertisesConfig config : this.CUSTOM_CONFIGS.values()) {
+            ModConfigs.EXPERTISES.tree.skills.addAll(config.tree.skills);
+        }
+    }
+}
