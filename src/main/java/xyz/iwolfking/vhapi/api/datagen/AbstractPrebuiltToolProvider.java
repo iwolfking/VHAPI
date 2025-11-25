b0d05988004f456ad7a7145fe631e1aadc77cf3e
iwolfking <thegreatiwk@gmail.com>
2025-11-25 15:55:08 -0600

    Fix issues with Prebuilt Tool provider

--- src/main/java/xyz/iwolfking/vhapi/api/datagen/AbstractPrebuiltToolProvider.java
+++ src/main/java/xyz/iwolfking/vhapi/api/datagen/AbstractPrebuiltToolProvider.java
@@ -3,12 +3,15 @@ package xyz.iwolfking.vhapi.api.datagen;
 import com.google.gson.JsonObject;
 import iskallia.vault.config.PreBuiltToolConfig;
 import iskallia.vault.config.TooltipConfig;
+import iskallia.vault.core.data.serializable.ISerializable;
+import iskallia.vault.core.world.roll.DoubleRoll;
 import iskallia.vault.core.world.roll.FloatRoll;
 import iskallia.vault.core.world.roll.IntRoll;
 import iskallia.vault.gear.attribute.VaultGearAttribute;
 import iskallia.vault.init.ModGearAttributes;
 import iskallia.vault.item.tool.ToolMaterial;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import xyz.iwolfking.vhapi.api.datagen.lib.VaultConfigBuilder;
 import xyz.iwolfking.vhapi.mixin.accessors.PrebuiltToolConfigAccessor;
@@ -56,32 +59,74 @@ public abstract class AbstractPrebuiltToolProvider extends AbstractVaultConfigDa
             private final Map<String, Object> implicits = new HashMap<>();
             private final Map<String, Object> prefixes = new HashMap<>();
 
-            public void material(ToolMaterial toolMaterial) {
+            public ToolAffixBuilder material(ToolMaterial toolMaterial) {
                 toolAffixes.put("tool_material", Map.of("value", toolMaterial.name()));
+                return this;
             }
 
-            public <T> void implicit(VaultGearAttribute<T> attribute, T value) {
+            public <T> ToolAffixBuilder implicit(VaultGearAttribute<T> attribute, T value) {
                 implicits.put(attribute.getRegistryName().toString(), value);
+                return this;
             }
 
-            public void implicit(VaultGearAttribute<Float> attribute, FloatRoll value) {
-                implicits.put(attribute.getRegistryName().toString(), value);
+            public ToolAffixBuilder implicit(VaultGearAttribute<Float> attribute, FloatRoll value) {
+                implicits.put(attribute.getRegistryName().toString(), wrapRollWithType(value));
+                return this;
             }
 
-            public void implicit(VaultGearAttribute<Integer> attribute, IntRoll value) {
-                implicits.put(attribute.getRegistryName().toString(), value);
+            public ToolAffixBuilder implicit(VaultGearAttribute<Integer> attribute, IntRoll value) {
+                implicits.put(attribute.getRegistryName().toString(), wrapRollWithType(value));
+                return this;
             }
 
-            public <T> void prefix(VaultGearAttribute<T> attribute, T value) {
+            public <T> ToolAffixBuilder prefix(VaultGearAttribute<T> attribute, T value) {
                 prefixes.put(attribute.getRegistryName().toString(), value);
+                return this;
             }
 
-            public void prefix(VaultGearAttribute<Float> attribute, FloatRoll value) {
-                prefixes.put(attribute.getRegistryName().toString(), value);
+            public <T extends Number> ToolAffixBuilder prefix(VaultGearAttribute<T> attribute, FloatRoll value) {
+                prefixes.put(attribute.getRegistryName().toString(), wrapRollWithType(value));
+                return this;
             }
 
-            public void prefix(VaultGearAttribute<Integer> attribute, IntRoll value) {
-                prefixes.put(attribute.getRegistryName().toString(), value);
+            public ToolAffixBuilder prefix(VaultGearAttribute<Integer> attribute, IntRoll value) {
+                prefixes.put(attribute.getRegistryName().toString(), wrapRollWithType(value));
+                return this;
+            }
+
+            private <T extends ISerializable<CompoundTag, JsonObject>> Object wrapRollWithType(T rollObject) {
+                JsonObject object = rollObject.writeJson().orElse(null);
+                if(object == null) {
+                    return new JsonObject();
+                }
+
+                if(rollObject instanceof IntRoll intRoll) {
+                    object.addProperty("roll_type", "int_roll");
+                    if(intRoll instanceof IntRoll.UniformedStep) {
+                        object.addProperty("type", "uniformed_step");
+                    }
+                    if(intRoll instanceof IntRoll.Uniform) {
+                        object.addProperty("type", "uniform");
+                    }
+                    if(intRoll instanceof IntRoll.Constant) {
+                        object.addProperty("type", "constant");
+                    }
+                }
+                else if(rollObject instanceof FloatRoll floatRoll) {
+                    object.addProperty("roll_type", "float_roll");
+                    if(floatRoll instanceof FloatRoll.UniformedStep) {
+                        object.addProperty("type", "uniformed_step");
+                    }
+                    if(floatRoll instanceof FloatRoll.Uniform) {
+                        object.addProperty("type", "uniform");
+                    }
+                    if(floatRoll instanceof FloatRoll.Constant) {
+                        object.addProperty("type", "constant");
+                    }
+                }
+
+                return object;
+
             }
 
             public Map<String, Map<String, Object>> build() {
--- src/main/java/xyz/iwolfking/vhapi/mixin/accessors/CompanionRelicsConfigAccessor.java
+++ src/main/java/xyz/iwolfking/vhapi/mixin/accessors/CompanionRelicsConfigAccessor.java
@@ -1,18 +1,13 @@
 package xyz.iwolfking.vhapi.mixin.accessors;
 
-import com.google.gson.annotations.Expose;
 import iskallia.vault.config.CompanionRelicsConfig;
-import iskallia.vault.config.SkillGatesConfig;
 import iskallia.vault.config.entry.LevelEntryList;
 import iskallia.vault.core.util.WeightedList;
-import iskallia.vault.skill.SkillGates;
 import iskallia.vault.util.VaultRarity;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
-
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Mixin(value = CompanionRelicsConfig.class, remap = false)
