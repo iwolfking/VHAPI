93444f42ddd0fe6ff803f164aaea95157950bab9
iwolfking <thegreatiwk@gmail.com>
2025-11-26 16:21:17 -0600

    Better ability tree merging handling

--- src/main/java/xyz/iwolfking/vhapi/api/loaders/skills/AbilitiesConfigLoader.java
+++ src/main/java/xyz/iwolfking/vhapi/api/loaders/skills/AbilitiesConfigLoader.java
@@ -7,6 +7,7 @@ import iskallia.vault.skill.base.Skill;
 import iskallia.vault.skill.base.SpecializedSkill;
 import iskallia.vault.skill.tree.AbilityTree;
 import iskallia.vault.skill.tree.SkillTree;
+import oshi.util.tuples.Pair;
 import xyz.iwolfking.vhapi.api.events.VaultConfigEvent;
 import xyz.iwolfking.vhapi.api.loaders.lib.core.VaultConfigProcessor;
 import xyz.iwolfking.vhapi.mixin.accessors.AbilitiesConfigAccessor;
@@ -27,9 +28,66 @@ public class AbilitiesConfigLoader extends VaultConfigProcessor<AbilitiesConfig>
     @Override
     public void afterConfigsLoad(VaultConfigEvent.End event) {
 
-        for(AbilitiesConfig config : this.CUSTOM_CONFIGS.values()) {
-            ModConfigs.ABILITIES.tree.skills.addAll(config.tree.skills);
-        }
+        this.CUSTOM_CONFIGS.forEach((resourceLocation, abilitiesConfig) -> {
+            if(resourceLocation.getPath().contains("remove")) {
+                List<Skill> skillsToRemove = new ArrayList<>();
+                abilitiesConfig.tree.skills.forEach(skill -> {
+                    if(ModConfigs.ABILITIES.getAbilityById(skill.getId()).isPresent()) {
+                        skillsToRemove.add(ModConfigs.ABILITIES.getAbilityById(skill.getId()).get());
+                    }
+                });
+                ModConfigs.ABILITIES.tree.skills.removeAll(skillsToRemove);
+            }
+            if(resourceLocation.getPath().contains("replace")) {
+                List<Skill> skillsToReplace = new ArrayList<>();
+                abilitiesConfig.tree.skills.forEach(skill -> {
+                    if(ModConfigs.ABILITIES.getAbilityById(skill.getId()).isPresent()) {
+                        skillsToReplace.add(ModConfigs.ABILITIES.getAbilityById(skill.getId()).get());
+                    }
+                });
+                ModConfigs.ABILITIES.tree.skills.forEach(skill -> {
+                    if(skillsToReplace.contains(skill)) {
+                        if(skill instanceof SpecializedSkill specializedSkill) {
+                            ((SpecializedSkillAccessor)specializedSkill).setSpecializations(((SpecializedSkillAccessor)abilitiesConfig.getAbilityById(skill.getId()).get()).getSpecializations());
+                        }
+                    }
+                });
+            }
+            if(resourceLocation.getPath().contains("add_specialization")) {
+                List<Skill> skillsToReplace = new ArrayList<>();
+                abilitiesConfig.tree.skills.forEach(skill -> {
+                    if(ModConfigs.ABILITIES.getAbilityById(skill.getId()).isPresent()) {
+                        skillsToReplace.add(ModConfigs.ABILITIES.getAbilityById(skill.getId()).get());
+                    }
+                });
+                ModConfigs.ABILITIES.tree.skills.forEach(skill -> {
+                    if(skillsToReplace.contains(skill)) {
+                        if(skill instanceof SpecializedSkill specializedSkill) {
+                            List<LearnableSkill> specs = ((SpecializedSkillAccessor)specializedSkill).getSpecializations();
+                            Skill newSkill = abilitiesConfig.getAbilityById(specializedSkill.getId()).orElse(null);
+                            if(newSkill instanceof SpecializedSkill newSpecializationSkill) {
+                                List<LearnableSkill> newSpecs = ((SpecializedSkillAccessor)newSpecializationSkill).getSpecializations();
+                                specs.removeIf(learnableSkill -> {
+                                    for(LearnableSkill skill1 : newSpecs) {
+                                        if(learnableSkill.getId().equals(skill1.getId())) {
+                                            return true;
+                                        }
+                                    }
+
+                                    return false;
+                                });
+                                specs.addAll(newSpecs);
+                            }
+                            ((SpecializedSkillAccessor)specializedSkill).setSpecializations(specs);
+                        }
+                    }
+                });
+            }
+            else {
+                ModConfigs.ABILITIES.tree.skills.addAll(abilitiesConfig.tree.skills);
+            }
+        });
+
 
         super.afterConfigsLoad(event);
     }
