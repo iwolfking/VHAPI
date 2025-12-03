fba52dbd0c4256084462e5584e8812dc47fb105d
iwolfking <thegreatiwk@gmail.com>
2025-12-03 14:59:08 -0600

    Fix AbstractVaultModifierProvider GSON

--- src/main/java/xyz/iwolfking/vhapi/api/datagen/AbstractVaultModifierProvider.java
+++ src/main/java/xyz/iwolfking/vhapi/api/datagen/AbstractVaultModifierProvider.java
@@ -1,15 +1,24 @@
 package xyz.iwolfking.vhapi.api.datagen;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import iskallia.vault.config.Config;
+import iskallia.vault.config.adapter.WeightedListAdapter;
+import iskallia.vault.core.data.adapter.Adapters;
+import iskallia.vault.core.data.adapter.util.IdentifierAdapter;
+import iskallia.vault.core.world.data.entity.EntityPredicate;
+import iskallia.vault.core.world.data.tile.TilePredicate;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.DataProvider;
 import net.minecraft.data.HashCache;
+import net.minecraft.resources.ResourceLocation;
 import xyz.iwolfking.vhapi.api.datagen.lib.modifiers.ModifierBuilder;
 import xyz.iwolfking.vhapi.api.datagen.lib.modifiers.ModifierFile;
 
 import java.io.IOException;
+import java.lang.reflect.Modifier;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.LinkedHashMap;
@@ -18,9 +27,11 @@ import java.util.function.Consumer;
 
 public abstract class AbstractVaultModifierProvider implements DataProvider {
 
-    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().registerTypeAdapter(ResourceLocation .class, new IdentifierAdapter(true)).registerTypeAdapterFactory(WeightedListAdapter.Factory.INSTANCE).registerTypeHierarchyAdapter(TilePredicate.class, Adapters.TILE_PREDICATE).registerTypeHierarchyAdapter(EntityPredicate.class, Adapters.ENTITY_PREDICATE).create();
     private final DataGenerator gen;
     private final String modid;
+
 
     public AbstractVaultModifierProvider(DataGenerator gen, String modid) {
         this.gen = gen;
