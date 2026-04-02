72f4b05539a333f93afd330499bcf371781c8351
iwolfking <thegreatiwk@gmail.com>
2026-04-02 16:19:43 -0500

    Fix mixin crash

--- src/main/java/xyz/iwolfking/vhapi/mixin/registry/gear/MixinGearModelRollRaritiesConfig.java
+++ src/main/java/xyz/iwolfking/vhapi/mixin/registry/gear/MixinGearModelRollRaritiesConfig.java
@@ -6,10 +6,12 @@ import iskallia.vault.config.Config;
 import iskallia.vault.config.GearModelRollRaritiesConfig;
 import iskallia.vault.dynamodel.DynamicModel;
 import iskallia.vault.dynamodel.model.armor.ArmorPieceModel;
+import iskallia.vault.dynamodel.registry.DynamicModelRegistry;
 import iskallia.vault.gear.VaultGearRarity;
 import iskallia.vault.init.ModDynamicModels;
 import iskallia.vault.item.gear.VaultArmorItem;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -23,13 +25,12 @@ import xyz.iwolfking.vhapi.api.data.api.CustomGearModelRolls;
 import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Mixin(value = GearModelRollRaritiesConfig.class, remap = false)
 public abstract class MixinGearModelRollRaritiesConfig extends Config {
 
-    @Shadow public abstract Map<String, List<String>> getRolls(ItemStack stack);
-
-    @Shadow @Nullable protected abstract VaultGearRarity getForcedTierRarity(ItemStack stack, ResourceLocation modelId);
+    @Shadow abstract Map<String, List<String>> getRolls(Item item);
 
     @Inject(method = "getRolls", at = @At("HEAD"), cancellable = true)
     private void getRollsHook(CallbackInfoReturnable<Map<String, List<String>>> cir, @Local ItemStack stack) {
@@ -38,38 +39,44 @@ public abstract class MixinGearModelRollRaritiesConfig extends Config {
         }
     }
 
-
     /**
      * @author iwolfking
      * @reason Allow for Vault Gear Rarity Maps
      */
     @Overwrite
-    public VaultGearRarity getRarityOf(ItemStack stack, ResourceLocation modelId) {
+    public VaultGearRarity getRarityOf(Item stack, ResourceLocation modelId) {
         Map<String, List<String>> rolls = this.getRolls(stack);
         if (rolls == null) {
             return VaultGearRarity.SCRAPPY;
         } else {
-            VaultGearRarity predefined = this.getForcedTierRarity(stack, modelId);
-            if (predefined != null) {
-                return predefined;
-            } else {
-                if (stack.getItem() instanceof VaultArmorItem) {
-                    modelId = (ResourceLocation) ModDynamicModels.Armor.PIECE_REGISTRY.get(modelId).map(ArmorPieceModel::getArmorModel).map(DynamicModel::getId).orElse(modelId);
-                }
-
-                for (int i = VaultGearRarity.values().length - 1; i >= 0; --i) {
-                    VaultGearRarity rarity = VaultGearRarity.values()[i];
-                    List<String> modelIds = (List) rolls.get(rarity.name());
-                    if(modelIds == null) {
-                        modelIds = rolls.get(rarity);
-                    }
-                    if (modelIds != null && modelIds.contains(modelId.toString())) {
-                        return rarity;
-                    }
-                }
-
-                return VaultGearRarity.SCRAPPY;
+            if (stack instanceof VaultArmorItem) {
+                modelId = (ResourceLocation) ModDynamicModels.Armor.PIECE_REGISTRY.get(modelId).map(ArmorPieceModel::getArmorModel).map(DynamicModel::getId).orElse(modelId);
             }
+
+            boolean isSpecial = false;
+            Optional<DynamicModelRegistry<?>> registry = ModDynamicModels.REGISTRIES.getAssociatedRegistry(stack);
+            if (registry.isPresent()) {
+                DynamicModelRegistry<?> reg = (DynamicModelRegistry) registry.get();
+                isSpecial = (Boolean) reg.get(modelId).map((m) -> m.getModelProperties().doesRequireRewards()).orElse(false);
+            }
+
+            if (isSpecial) {
+                return VaultGearRarity.SPECIAL;
+            }
+
+            for (int i = VaultGearRarity.values().length - 1; i >= 0; --i) {
+                VaultGearRarity rarity = VaultGearRarity.values()[i];
+                List<String> modelIds = (List) rolls.get(rarity.name());
+                if (modelIds == null) {
+                    modelIds = rolls.get(rarity);
+                }
+                if (modelIds != null && modelIds.contains(modelId.toString())) {
+                    return rarity;
+                }
+            }
+
+            return VaultGearRarity.SCRAPPY;
         }
     }
+
 }
