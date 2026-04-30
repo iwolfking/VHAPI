d79da590fa80fd77947ff30693bc1e1aad73cfcf
iwolfking <thegreatiwk@gmail.com>
2026-04-29 21:06:14 -0500

    Improve modifier support in challenge crystal provider

--- src/main/java/xyz/iwolfking/vhapi/api/datagen/AbstractChallengeCrystalProvider.java
+++ src/main/java/xyz/iwolfking/vhapi/api/datagen/AbstractChallengeCrystalProvider.java
@@ -6,6 +6,9 @@ import iskallia.vault.config.TooltipConfig;
 import iskallia.vault.config.VaultCrystalConfig;
 import iskallia.vault.core.vault.hazard.Hazard;
 import iskallia.vault.core.vault.modifier.VaultModifierStack;
+import iskallia.vault.core.vault.modifier.modifier.EmptyModifier;
+import iskallia.vault.core.vault.modifier.registry.VaultModifierRegistry;
+import iskallia.vault.core.vault.modifier.spi.VaultModifier;
 import iskallia.vault.core.world.roll.IntRoll;
 import iskallia.vault.item.crystal.hazard.CrystalHazards;
 import iskallia.vault.item.crystal.layout.ClassicInfiniteCrystalLayout;
@@ -19,8 +22,12 @@ import iskallia.vault.item.crystal.theme.CrystalTheme;
 import iskallia.vault.item.crystal.theme.PoolCrystalTheme;
 import iskallia.vault.item.crystal.time.CrystalTime;
 import iskallia.vault.item.crystal.time.ValueCrystalTime;
+import net.minecraft.ChatFormatting;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.TextColor;
 import net.minecraft.resources.ResourceLocation;
+import xyz.iwolfking.vhapi.api.datagen.lib.IGenericBuilder;
 import xyz.iwolfking.vhapi.api.datagen.lib.VaultConfigBuilder;
 import xyz.iwolfking.vhapi.mixin.accessors.ChallengeCrystalConfigAccessor;
 import xyz.iwolfking.vhapi.mixin.accessors.TooltipConfigAccessor;
@@ -100,6 +107,19 @@ public abstract class AbstractChallengeCrystalProvider extends AbstractVaultConf
 
         public ChallengeEntryBuilder modifier(VaultModifierStack modifier) {
             this.modifiers.add(modifier);
+            return this;
+        }
+
+        public ChallengeEntryBuilder modifier(ResourceLocation id, int size) {
+            CompoundTag tag = new CompoundTag();
+            tag.putString("id", id.toString());
+            tag.putInt("size", size);
+            //Why you make me do this :(, fake that modifier is registered so VaultModifierStack is valid, only called in datagen so no problem
+            if(VaultModifierRegistry.getOpt(id).isEmpty()) {
+                VaultModifierRegistry.register(id, new EmptyModifier(id, new EmptyModifier.Properties(), new VaultModifier.Display("", TextColor.fromLegacyFormat(ChatFormatting.AQUA), "")));
+            }
+            this.modifiers.add(VaultModifierStack.of(tag));
+
             return this;
         }
 
